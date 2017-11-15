a62358b6ebb330c7f64db12d532b258c32a41d81
Daniel Duton <daniel@duton.lu>
2017-11-15 16:57:53 +0100

    add server build script

---
+++ build/build-server.js
@@ -0,0 +1,26 @@
+const { transformFileSync } = require('babel-core')
+const glob = require('glob')
+const fs = require('fs-extra')
+const path = require('path')
+
+const from = path.resolve(__dirname, '../src')
+const to = path.resolve(__dirname, '../dist/server')
+
+fs.remove(to)
+.then(() => fs.copy(from, to))
+.then(() => {
+    glob(path.resolve(to, '**/*.js'), (err, files) => {
+        files.forEach(file => {
+            let { code } = transformFileSync(file, {
+                presets: [
+                    ['env', {
+                        targets: {
+                            node: '6.10'
+                        }
+                    }]
+                ]
+            })
+            fs.outputFile(file, code)
+        })
+    })
+})
