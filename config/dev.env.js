fe67935c388e641894180010429219e47d7b619e
Daniel Duton <daniel@duton.lu>
2017-11-16 13:36:23 +0100

    fix env vars

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  SOCKET_SERVER: "//localhost:4200"
+  SOCKET_SERVER: '"//localhost:4200"'
 })
